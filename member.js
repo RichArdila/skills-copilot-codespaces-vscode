46e4c475485ccf5b90af3c441f672d874bfdce2d
Richard Ardila Zapata <139007449+RichArdila@users.noreply.github.com>
2023-11-21 03:22:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  if (document.getElementById("skills").style.display === "none") {
+    document.getElementById("skills").style.display = "block";
+  } else {
+    document.getElementById("skills").style.display = "none";
+  }
+}
